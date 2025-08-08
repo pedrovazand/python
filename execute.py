136d80162d4983c583158d317fb68fa12b008304
pedrohenriquevaz <pedro.andrade@siscobra.com.br>
2025-08-08 08:47:54 -0300

    Initial commit

--- execute.py
+++ execute.py
@@ -3,6 +3,8 @@ from db_utils import execute_sql
 with open("./sql/correcao.sql", "r", encoding="UTF-8") as file:
         sql = file.read()
 
-ports = [7007, 7015, 7029]
+#7001, 7002, 7003, 7005, 7006, 7007, 7008, 7010, 7011, 7012, 7014, 7015, 7017, 7018, 7019, 7020, 7021, 7022, 7023, 7024, 7026, 7027, 7028, 7029, 7030, 7031
+
+ports = []
 
 execute_sql(sql, ports)
--- sql/correcao.sql
+++ sql/correcao.sql
@@ -1,29 +1,48 @@
-CREATE FUNCTION parcelas_pagas(_tipo int, _identificador int8)
-RETURNS bigint 
-LANGUAGE SQL 
-AS $$
+CREATE OR REPLACE FUNCTION public.devedor_devati()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+	_vencimento_recente DATE;
+	_vencimento_antigo DATE;
+BEGIN
+	IF (tg_op = 'UPDATE') THEN
+		SELECT COALESCE(min(cp.conpardatven),'0001-01-01'::date), 
+			   COALESCE(max(cp.conpardatven),'0001-01-01'::date)
+		  INTO _vencimento_antigo, 
+			   _vencimento_recente 
+		  FROM contrato con 
+			  JOIN contrato_parcela cp ON con.concod = cp.concod AND cp.conparvalsal > 0
+		 WHERE con.carcod = NEW.carcod
+		   AND con.devcod = NEW.devcod;
+											
+		NEW.devvenmaisantigo = _vencimento_antigo;
+		NEW.devvenmaisrecente = _vencimento_recente;
+	
+		IF COALESCE(NEW.devsal, 0) <> COALESCE(OLD.devsal, 1) THEN
+			IF NEW.devsal <= 0 THEN
+				IF OLD.devati = 0 THEN
+					NEW.devati = 1;
+				END IF;
 
-	 SELECT CASE 
-			 	WHEN _tipo = 1 THEN (SELECT count(cpr.conparseq)
-			 						   FROM contrato con 
-			 						  	   JOIN contrato_parcela cp ON con.concod = cp.concod 
-			 						  	   JOIN contrato_parcela_retomada cpr ON cp.conparseq = cpr.conparseq
-			 						  WHERE cpr.conparretati = 0
-			 						    AND cpr.tipenccod = 1
-			 						    AND con.devcod = _identificador) -- Devedor
-			 	
-			 	WHEN _tipo = 2 THEN (SELECT count(cpr.conparseq)
-			 						   FROM contrato_parcela cp 
-			 						   	   JOIN contrato_parcela_retomada cpr ON cp.conparseq = cpr.conparseq 
-			 						  WHERE cpr.conparretati = 0
-			 						    AND cpr.tipenccod = 1
-			 						    AND cp.concod = _identificador) -- Contrato
-			 						 
-			 	WHEN _tipo = 3 THEN (SELECT count(apr.acoparretseq)
-									   FROM acordo_parcela ap	
-									   	   JOIN acordo_parcela_retomada apr ON ap.acoparseq = apr.acoparseq 
-									  WHERE apr.acoparretati = 0
-									    AND apr.tipenccod = 1
-									    AND ap.acocod = _identificador) -- Acordo
-			 END;
-$$;
+				IF COALESCE(OLD.devdatpridev, '0001-01-01'::date) = '0001-01-01'::date then
+					NEW.devdatpridev = current_date;
+				END IF;
+			ELSE
+				IF OLD.devati = 1 THEN
+					NEW.devati = 0;
+				END IF;
+			END IF;	
+		END IF;
+	END IF;
+RETURN NEW; 
+END;
+$function$
+; 
+
+DROP TRIGGER ativa_inativa_devedor ON devedor;
+
+CREATE TRIGGER ativa_inativa_devedor BEFORE
+UPDATE
+    ON
+    public.devedor FOR EACH ROW EXECUTE PROCEDURE devedor_devati();
